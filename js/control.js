7df30bcba8f0a3567fb00d7b848b8f6edad30f56
vinhky214 <140496837+vinhky214@users.noreply.github.com>
2023-08-05 17:45:00 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+$('#sp-men').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('#sp-women').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('#sp-kid').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
